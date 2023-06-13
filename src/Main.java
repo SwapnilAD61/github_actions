b053ba8143a4615823db413f7b16a7e8f74553f9
SwapnilAD61 <swapnil.deoraye@fineshift.com>
2023-06-13 10:23:54 +0530

    1st

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Swapnil Deoraye");
+        int arr[] = {10, 20, 30, 40, 50};
+
+        int n=arr.length;
+        int start = 0, end = n-1;
+        System.out.println("swapnil");
+        while(start<end){
+            int temp = arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+        for(int i=0; i<n; i++)
+            System.out.print(arr[i]+" ");
     }
 }
